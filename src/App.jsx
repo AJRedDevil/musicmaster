4231057eb14535155b0f35722f8f715cde4e71e5
AJRedDevil <ajan.shresh@gmail.com>
2017-09-07 16:51:39 +0545

    Fetching data from url

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,19 @@ class App extends Component {
     search() {
         // console.log('this.state', this.state);
         const BASE_URL = 'https://api.spotify.com/v1/search?';
-        const FETCH_URL = `${BASE_URL}'q='${this.state.query}&type=artist&limit=1'`;
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
         console.log('FETCH_URL ' + FETCH_URL);
+        const access_token = 'BQA7kNWA2mnKCnTtBl-ePFYuFCC42RwtmA397NCMASRBScvvQLt7Mwp_y-wISpZyPdQciplAC9JvD0TRejxSC-NxpchEjdUzVLc4eSfn8nFM2rsg7oiAJZsuPSovfEf3-NNxQMcYGSQOzKELxkGd0jPvbolkBsA';
+        // Authorization: 'Bearer ' + access_token
+        fetch(FETCH_URL, {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + access_token
+            }
+        })
+        .then(response => response.json())
+        .then(json => console.log('json ', json))
+        .catch(error => console.log(error));
     }
 
     render() {
