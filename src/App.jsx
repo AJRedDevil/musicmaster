024174c1af27bbc74b39a2e11e9c6cd57b91cf10
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-09-08 14:27:31 +0545

    Added Basic skeleton profile from fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 import './App.css';
+import Profile from './Profile';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            query: ''
+            query: '',
+            artist: null
         };
         this.search = this.search.bind(this);
     }
@@ -17,8 +18,7 @@ class App extends Component {
         const BASE_URL = 'https://api.spotify.com/v1/search?';
         const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
         console.log('FETCH_URL ' + FETCH_URL);
-        const access_token = 'BQA7kNWA2mnKCnTtBl-ePFYuFCC42RwtmA397NCMASRBScvvQLt7Mwp_y-wISpZyPdQciplAC9JvD0TRejxSC-NxpchEjdUzVLc4eSfn8nFM2rsg7oiAJZsuPSovfEf3-NNxQMcYGSQOzKELxkGd0jPvbolkBsA';
-        // Authorization: 'Bearer ' + access_token
+        const access_token = 'BQDivyxKYy9cBVfywjnSg5_aA--7h8OutDZhHIB4E4mQhyrue1hyWOhfkIiOPeRWvsMIm5Hx1Zs__xJJ9ENdSXqF97OH9sV_kqKJycfzm0voET6s0GPlxrFXmFBq1aPiB3LHimlfWq3mOswD5qNFZRN3t3URJzg&refresh_token=AQCm8AlLwjNhCqo742zbk4yeL42Z6ofBn0VhWu4k58vaciXTpHIfqLYnkB9imkjB68Ed2u1x1E3fbz4ZGtZdlUdDBlgLsj6cIgYTeU27M2uTTDjb_ROTa7XUkmu26AF2rvQ';
         fetch(FETCH_URL, {
             method: 'GET',
             headers: {
@@ -26,7 +26,11 @@ class App extends Component {
             }
         })
         .then(response => response.json())
-        .then(json => console.log('json ', json))
+        .then(json => {
+            const artist = json.artists.items[0];
+            console.log('artist', artist);
+            this.setState({artist});
+        })
         .catch(error => console.log(error));
     }
 
@@ -52,6 +56,9 @@ class App extends Component {
                         </InputGroup.Addon>
                     </InputGroup>
                 </FormGroup>
+                <Profile
+                    artist={this.state.artist}
+                />
                 <div className="Profile">
                     <div>Artist Pictures</div>
                     <div>Artist Name</div>
